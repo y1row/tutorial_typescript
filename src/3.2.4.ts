cf3a68dd0fed38b0276fca5b18cf64b989423176
y1row <y1row.kato@gmail.com>
2025-10-05 14:09:57 +0900

    3.2.4

---
+++ src/3.2.4.ts
@@ -0,0 +1,10 @@
+let int = 1234 // 整数
+let inf = Infinity * 0.1
+const constNum = 5678
+//constNum = 2345 // TS2588: Cannot assign to 'constNum' because it is a constant.
+
+let float: 26.128 = 26.128
+
+// カンマ区切りでも書ける
+let oneMillion = 1_000_000
+let twoThousand: 2_000 = 2_000
