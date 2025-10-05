bfda4eba4eda484dfd904485c04cd801fd66e7cf
y1row <y1row.kato@gmail.com>
2025-10-05 13:25:48 +0900

    2.2

---
+++ src/2.2.ts
@@ -0,0 +1,13 @@
+// アノテーション
+let a: number = 1
+let b: string = 'hello'
+let c: boolean[] = [true, false]
+
+console.log(a, b, c)
+
+// 型推論
+let d = 1
+let e = 'hello'
+let f = [true, false]
+
+console.log(d, e, f)
